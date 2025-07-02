2c4cf338dd6563c5a2265a467ba6b6500d063625
Sathish Thiyagarajan <sathish.thiyagarajan@earlham.ac.uk>
2025-07-02 17:30:57 +0100

    Updates mainly to read_stats

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # `nanalogue`
 
-TBD
+Package to primarily process single-molecule base modification data
+stored in the modBAM format.
+
+# Commands
+
+### `nanalogue bc-len-vs-align-len`
+
+Given a BAM file and a sequencing summary file, or just the BAM file,
+this command outputs a table where each row contains the basecalled
+length and alignment length per molecule.
+
+### `nanalogue read-stats`
+
+Given a BAM file, prints read and alignment length statistics such as N50,
+and how many alignments are primary, secondary etc. For more statistics,
+please consider using `samtools stats`.
--- src/subcommands/read_stats.rs
+++ src/subcommands/read_stats.rs
@@ -39,9 +39,59 @@ impl CurrRead {
 	}
 }
 
+fn get_stats_from_heap(mut input: BinaryHeap::<u64>, total_length: u64) -> (u64, f32, u64, u64, u64, u64){
+    // process heaps to get statistics
+    let mut counter :u64 = 0;
+    let mut longest :u64 = 0;
+    let mut shortest :u64 = 0;
+    let mut median :u64 = 0;
+    let mut n50 :u64 = 0;
+    let mut running_total_length :u64 = 0;
+    let heap_size :u64 = match input.len().try_into(){
+        Ok(v) => v,
+        Err(_) => {
+            eprintln!("Error while getting heap length. Is it too large?");
+            std::process::exit(1);
+        },
+    };
+
+    loop {
+        match input.pop() {
+            Some(v) => {
+
+                if counter == 0 { longest = v };
+
+                running_total_length += v;
+
+                if median == 0 && counter > heap_size / 2 {
+                    median = v;
+                }
+
+                if n50 == 0 && running_total_length > total_length / 2 {
+                    n50 = v;
+                }
+
+                shortest = v;
+
+                counter += 1;
+            },
+            None => break,
+        };
+    };
+
+    assert_eq!(running_total_length, total_length);
+
+    let mean = match counter {
+        0 => 0.0,
+        v => (total_length as f32) / (v as f32 * 1.0),
+    };
+
+    (counter, mean, longest, shortest, median, n50)
+}
+
 pub fn run(bam_path: &str) -> Result<(), Box<dyn Error>> {
 
-	// open BAM file
+    // open BAM file
     let mut bam = match bam::Reader::from_path(bam_path) {
         Ok(v) => {
             v
@@ -61,6 +111,10 @@ pub fn run(bam_path: &str) -> Result<(), Box<dyn Error>> {
     // create two read length heaps
     let mut seq_len_heap = BinaryHeap::<u64>::new();
     let mut align_len_heap = BinaryHeap::<u64>::new();
+
+    // total read length variables
+    let mut seq_len_total = 0;
+    let mut align_len_total = 0;
 
     // Go record by record in the BAM file,
     for r in bam.records() {
@@ -88,7 +142,7 @@ pub fn run(bam_path: &str) -> Result<(), Box<dyn Error>> {
             ReadState::Unmapped => unmapped_count += 1,
         };
 
-		// get length of alignment
+        // get length of alignment
         match curr_read_state.state {
             ReadState::Unmapped => {},
             _ => {
@@ -100,6 +154,7 @@ pub fn run(bam_path: &str) -> Result<(), Box<dyn Error>> {
                     },
                 };
                 align_len_heap.push(align_len);
+                align_len_total += align_len;
             },
         };
 
@@ -112,16 +167,31 @@ pub fn run(bam_path: &str) -> Result<(), Box<dyn Error>> {
             },
         };
         seq_len_heap.push(seq_len);
+        seq_len_total += seq_len;
 	}
 
-    println!("Number of primary reads: {primary_count}");
-    println!("Number of secondary reads: {secondary_count}");
-    println!("Number of supplementary reads: {supplementary_count}");
-    println!("Number of unmapped reads: {unmapped_count}");
+    // process heaps to get statistics 
+    let (_, seq_len_mean, seq_len_max, seq_len_min, seq_len_median, seq_len_n50) = 
+        get_stats_from_heap(seq_len_heap, seq_len_total);
+    let (_, align_len_mean, align_len_max, align_len_min, align_len_median, align_len_n50) = 
+        get_stats_from_heap(align_len_heap, align_len_total);
 
-    if let Some(v) = align_len_heap.pop() { println!("Longest alignment length: {v}") }
-
-    if let Some(v) = seq_len_heap.pop() { println!("Longest sequence length: {v}") }
+    println!("# input bam {}", bam_path);
+    println!("key\tvalue");
+    println!("n_primary_alignments\t{primary_count}");
+    println!("n_secondary_alignments\t{secondary_count}");
+    println!("n_supplementary_alignments\t{supplementary_count}");
+    println!("n_unmapped_reads\t{unmapped_count}");
+    println!("align_len_mean\t{align_len_mean}");
+    println!("align_len_max\t{align_len_max}");
+    println!("align_len_min\t{align_len_min}");
+    println!("align_len_median\t{align_len_median}");
+    println!("align_len_n50\t{align_len_n50}");
+    println!("seq_len_mean\t{seq_len_mean}");
+    println!("seq_len_max\t{seq_len_max}");
+    println!("seq_len_min\t{seq_len_min}");
+    println!("seq_len_median\t{seq_len_median}");
+    println!("seq_len_n50\t{seq_len_n50}");
 
     Ok(())
 }
