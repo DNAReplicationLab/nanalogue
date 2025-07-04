5681947cd3090d17df92bec952a49508b262b5f8
Sathish Thiyagarajan <sathish.thiyagarajan@earlham.ac.uk>
2025-07-04 18:48:05 +0100

    Fixed git non-update mistake

---
+++ src/subcommands/read_info.rs
@@ -0,0 +1,84 @@
+use rust_htslib::{bam, bam::Read, bam::ext::BamRecordExtensions};
+use std::error::Error;
+use crate::{ReadState, ReadTransition, CurrRead};
+
+pub fn run(bam_path: &str, read_id: &str) -> Result<(), Box<dyn Error>> {
+
+    // open BAM file
+    let mut bam = match bam::Reader::from_path(bam_path) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("Problem opening file, error: {e}");
+            std::process::exit(1)
+        },
+    };
+
+    let mut output_string = String::from("");
+
+    // Go record by record in the BAM file,
+    for r in bam.records() {
+
+        // read records
+        let record = match r {
+            Ok(v) => {
+                // get read id
+                let qname :String = match str::from_utf8(v.qname()) {
+                    Ok(w) => w.to_string(),
+                    Err(e) => {
+                        eprintln!("Invalid UTF-8 sequence: {e}");
+                        std::process::exit(1)
+                    },
+                };
+                if qname == read_id { v }
+                else { continue; }
+            },
+            Err(e) => {
+                eprintln!("Some error while reading records {e}");
+                std::process::exit(1)
+            },
+        };
+
+        // set the read state using the type of alignment
+        let mut curr_read_state = CurrRead::new();
+        curr_read_state.read_id = Some(read_id.to_string());
+        if record.is_unmapped() { curr_read_state.transition(ReadTransition::PrimaryToUnmapped); }
+        if record.is_secondary() { curr_read_state.transition(ReadTransition::PrimaryToSecondary); }
+        if record.is_supplementary() { curr_read_state.transition(ReadTransition::PrimaryToSupplementary); }
+
+        // get length of sequence
+        let seq_len :u64 = match record.seq_len().try_into() {
+            Ok(v) => v,
+            Err(_) => {
+                eprintln!("Error while getting sequencing length!");
+                std::process::exit(1);
+            },
+        };
+        curr_read_state.seq_len = Some(seq_len);
+
+        // get length of alignment
+        match curr_read_state.state {
+            ReadState::Unmapped => {},
+            _ => {
+                let align_len :u64 = match (record.reference_end() - record.pos()).try_into() {
+                    Ok(v) => v,
+                    Err(_) => {
+                        eprintln!("Problem getting alignment length");
+                        std::process::exit(1);
+                    },
+                };
+                curr_read_state.align_len = Some(align_len);
+            },
+        };
+
+        output_string = output_string + &curr_read_state.to_string() + "\n";
+
+	}
+    
+    if !output_string.is_empty() {
+        println!("{}", CurrRead::header_string());
+        print!("{output_string}");
+    }
+
+    Ok(())
+}
+
