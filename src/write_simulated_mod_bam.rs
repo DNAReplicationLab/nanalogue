3435653053cd95cd4e21986594ce1850814188d0
thiyagar <sathish.thiyagarajan@earlham.ac.uk>
2025-10-05 12:59:38 +0100

    creates index, sorts reads, adds adjustments

--- src/write_simulated_mod_bam.rs
+++ src/write_simulated_mod_bam.rs
@@ -38,6 +38,7 @@ use std::fs::File;
 use std::io::Write;
 use std::num::{NonZeroU32, NonZeroU64};
 use std::ops::RangeInclusive;
+use std::path::Path;
 use uuid::Uuid;
 
 /// Main configuration struct for simulation
@@ -170,9 +171,10 @@ fn generate_contigs<R: Rng>(
 }
 
 /// Writes contigs to a FASTA file
-fn write_fasta<I>(contigs: I, output_path: &str) -> Result<(), Error>
+fn write_fasta<I, J>(contigs: I, output_path: &J) -> Result<(), Error>
 where
     I: IntoIterator<Item = (String, Vec<u8>)>,
+    J: AsRef<Path> + ?Sized,
 {
     let mut file = File::create(output_path)?;
     for contig in contigs {
@@ -228,18 +230,22 @@ fn generate_reads<R: Rng>(
             let mut record = bam::Record::new();
             let qname = format!("{}", Uuid::new_v4()).into_bytes();
             record.unset_flags();
-            record.set_flags({
-                let random_state: ReadState = random();
-                u16::try_from(random_state)?
-            });
-            if !record.is_unmapped() {
-                let cigar = CigarString(vec![Cigar::Match(read_len as u32)]);
-                record.set(&qname, Some(&cigar), &read_seq, &qual);
-                record.set_mapq(mapq);
-                record.set_tid(contig_idx as i32);
-                record.set_pos(start_pos as i64);
-            } else {
-                record.set(&qname, None, &read_seq, &qual);
+            let random_state: ReadState = random();
+            record.set_flags(u16::try_from(random_state)?);
+            match random_state {
+                ReadState::Unmapped => {
+                    record.set(&qname, None, &read_seq, &qual);
+                    record.set_mapq(255);
+                    record.set_tid(-1);
+                    record.set_pos(-1);
+                }
+                _ => {
+                    let cigar = CigarString(vec![Cigar::Match(read_len as u32)]);
+                    record.set(&qname, Some(&cigar), &read_seq, &qual);
+                    record.set_mapq(mapq);
+                    record.set_tid(contig_idx as i32);
+                    record.set_pos(start_pos as i64);
+                }
             }
             record.set_mpos(-1);
             record.set_mtid(-1);
@@ -252,12 +258,24 @@ fn generate_reads<R: Rng>(
     Ok(reads)
 }
 
-/// Writes BAM file with reads
-fn write_bam<I, J, K>(reads: I, contigs: J, read_groups: K, output_path: &str) -> Result<(), Error>
+/// Writes a new BAM file with reads.
+/// Although this function can be used for other tasks like subsetting
+/// BAM files, this is not advised as the history of the BAM file
+/// stored in its header would be lost as we are generating a new header here.
+/// Reads have to be sorted or you'll get an error while making the index.
+fn write_denovo_bam<I, J, K, L, M>(
+    reads: I,
+    contigs: J,
+    read_groups: K,
+    comments: L,
+    output_path: &M,
+) -> Result<(), Error>
 where
     I: IntoIterator<Item = bam::Record>,
     J: IntoIterator<Item = (String, usize)>,
     K: IntoIterator<Item = String>,
+    L: IntoIterator<Item = String>,
+    M: AsRef<Path> + ?Sized,
 {
     let header = {
         let mut header = Header::new();
@@ -278,15 +296,19 @@ where
                     .push_tag(b"PU", "blank"),
             );
         }
-        let _ = header.push_comment(b"simulated BAM file, not real data");
+        for k in comments {
+            let _ = header.push_comment(k.as_bytes());
+        }
         header
     };
 
     let mut writer = bam::Writer::from_path(output_path, &header, bam::Format::Bam)?;
-
     for read in reads {
         writer.write(&read)?;
     }
+    drop(writer);
+
+    bam::index::build(output_path, None, bam::index::Type::Bai, 2)?;
 
     Ok(())
 }
@@ -324,19 +346,21 @@ pub fn run(
     let mut rng = rand::rng();
 
     let contigs = generate_contigs(config.contigs.number, config.contigs.len_range, &mut rng);
-    let read_groups: Vec<String> = (0..config.reads.len()).map(|k| format!("{0}", k)).collect();
+    let read_groups: Vec<String> = (0..config.reads.len()).map(|k| k.to_string()).collect();
     let reads = {
         let mut temp_reads = Vec::new();
         for k in config.reads.into_iter().zip(read_groups.clone()) {
             temp_reads.append(&mut generate_reads(&contigs, k.0, &k.1, &mut rng)?);
         }
+        temp_reads.sort_by_key(|k| (k.is_unmapped(), k.tid(), k.pos(), k.is_reverse()));
         temp_reads
     };
 
-    write_bam(
+    write_denovo_bam(
         reads,
         contigs.iter().map(|k| (k.name.clone(), k.seq.len())),
         read_groups,
+        vec![String::from("simulated BAM file, not real data")],
         bam_output_path,
     )?;
     write_fasta(
@@ -352,6 +376,7 @@ mod tests {
     use super::*;
     use rust_htslib::bam::Read;
 
+    /// Test for generation of random DNA of a given length
     #[test]
     fn test_generate_random_dna_sequence() {
         let seq = generate_random_dna_sequence(NonZeroU64::new(100).unwrap(), &mut rand::rng());
@@ -361,6 +386,7 @@ mod tests {
         }
     }
 
+    /// Tests contig generation
     #[test]
     fn test_generate_contigs() {
         let config = ContigConfig {
@@ -372,13 +398,14 @@ mod tests {
         assert_eq!(contigs.len(), 5);
         for (i, contig) in contigs.iter().enumerate() {
             assert_eq!(contig.name, format!("contig_{}", i));
-            assert!(contig.seq.len() >= 100 && contig.seq.len() <= 200);
+            assert!((100..=200).contains(&contig.seq.len()));
             for base in contig.seq.clone() {
                 assert!([b'A', b'C', b'G', b'T'].contains(&base));
             }
         }
     }
 
+    /// Tests writing to a fasta file and check its contents
     #[test]
     fn test_write_fasta() {
         let contigs = vec![
@@ -401,6 +428,7 @@ mod tests {
         std::fs::remove_file(&temp_path).expect("no error");
     }
 
+    /// Tests Read generation with desired properties.
     #[test]
     fn test_generate_reads() {
         let mut rng = rand::rng();
@@ -418,23 +446,26 @@ mod tests {
         let config = ReadConfig {
             number: NonZeroU32::new(10).unwrap(),
             mapq_range: OrdPair::new(10, 20).unwrap(),
-            base_qual_range: OrdPair::new(10, 20).unwrap(),
+            base_qual_range: OrdPair::new(30, 50).unwrap(),
             len_range: OrdPair::new(F32Bw0and1::new(0.2).unwrap(), F32Bw0and1::new(0.8).unwrap())
                 .unwrap(),
             mods: vec![],
         };
 
         let reads = generate_reads(&contigs, config, "1", &mut rng).unwrap();
-        assert!(reads.len() <= 10);
+        assert_eq!(reads.len(), 10);
 
         for read in &reads {
             if !read.is_unmapped() {
-                assert!(read.mapq() >= 10 && read.mapq() <= 20);
-                assert!(read.tid() >= 0 && read.tid() < 2);
+                assert!((4..=16).contains(&read.seq_len()));
+                assert!((10..=20).contains(&read.mapq()));
+                assert!((0..2).contains(&read.tid()));
+                assert!(read.qual().iter().all(|x| (30..=50).contains(x)));
             }
         }
     }
 
+    /// Tests full BAM creation
     #[test]
     fn test_full_bam_generation() {
         let config_json = r#"{
@@ -443,7 +474,7 @@ mod tests {
                 "len_range": [100, 200]
             },
             "reads": [{
-                "number": 5,
+                "number": 1000,
                 "mapq_range": [10, 20],
                 "base_qual_range": [10, 20],
                 "len_range": [0.1, 0.8],
@@ -451,27 +482,20 @@ mod tests {
             }]
         }"#;
 
+        // create files with random names and check for existence
         let bam_path = format!("/tmp/{}.bam", Uuid::new_v4());
         let fasta_path = format!("/tmp/{}.fa", Uuid::new_v4());
-
         assert!(run(config_json, &bam_path, &fasta_path).unwrap());
+        assert!(Path::new(&bam_path).exists());
+        assert!(Path::new(&fasta_path).exists());
 
-        assert!(std::path::Path::new(&bam_path).exists());
-        assert!(std::path::Path::new(&fasta_path).exists());
-
+        // read BAM file and check contig, read count
         let mut reader = bam::Reader::from_path(&bam_path).unwrap();
         let header = reader.header();
         assert_eq!(header.target_count(), 2);
+        assert_eq!(reader.records().count(), 1000);
 
-        let read_count = {
-            let mut cnt = 0;
-            for _ in reader.records() {
-                cnt += 1;
-            }
-            cnt
-        };
-        assert!(read_count <= 5);
-
+        // delete files
         std::fs::remove_file(bam_path).unwrap();
         std::fs::remove_file(fasta_path).unwrap();
     }
