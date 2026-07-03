264820e436780004f1840022fb5c6eaa9fe1fe6a
thiyagar <sathish.thiyagarajan@earlham.ac.uk>
2026-07-03 17:22:16 +0100

    adds reads_table zig files and infrastructure
    * added reads_table.zig - this reports a sequence table from BAM/CRAM file
    * added Cargo.toml zig_htslib_exploration exclude
    * added zig-out, .zig-cache excludes to .gitignore
    * added the htslib submodule to .gitmodules
    * added build.zig to build reads_table

---
+++ zig_htslib_exploration/build.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize: std.builtin.OptimizeMode = .ReleaseSafe;
+
+    const exe = b.addExecutable(.{
+        .name = "reads_table",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("reads_table.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    exe.root_module.addIncludePath(.{ .cwd_relative = "htslib" });
+    exe.root_module.addLibraryPath(.{ .cwd_relative = "htslib" });
+    exe.root_module.addRPath(.{ .cwd_relative = "htslib" });
+
+    exe.root_module.linkSystemLibrary("hts", .{});
+    exe.root_module.linkSystemLibrary("z", .{});
+    exe.root_module.linkSystemLibrary("bz2", .{});
+    exe.root_module.linkSystemLibrary("lzma", .{});
+    exe.root_module.linkSystemLibrary("deflate", .{});
+    exe.root_module.linkSystemLibrary("curl", .{});
+    exe.root_module.linkSystemLibrary("crypto", .{});
+    exe.root_module.linkSystemLibrary("pthread", .{});
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| run_cmd.addArgs(args);
+
+    const run_step = b.step("run", "Run reads_table");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ zig_htslib_exploration/reads_table.zig
@@ -0,0 +1,115 @@
+const std = @import("std");
+const c = @cImport({
+    @cInclude("htslib/sam.h");
+    @cInclude("htslib/hts.h");
+});
+const assert = std.debug.assert;
+
+// Prefix view of htslib's bam1_t.
+// We use this because @cImport makes bam1_t opaque due to bitfields.
+// This must match the leading layout of bam1_t in the htslib version we build against.
+// TODO: Add a comptime check that bam1_t's definition did not change in htslib.
+const Bam1CoreView = extern struct {
+    core: c.bam1_core_t,
+    id: u64,
+    data: [*]u8,
+    l_data: i32,
+    m_data: u32,
+    _unused_bitfield_storage: u32,
+};
+
+const Alignment_types = enum {
+    primary_forward,
+    primary_reverse,
+    secondary_forward,
+    secondary_reverse,
+    supplementary_forward,
+    supplementary_reverse,
+    unmapped,
+
+    pub fn name(self: Alignment_types) []const u8 {
+        return switch (self) {
+            Alignment_types.primary_forward => "primary_forward",
+            Alignment_types.primary_reverse => "primary_reverse",
+            Alignment_types.secondary_forward => "secondary_forward",
+            Alignment_types.secondary_reverse => "secondary_reverse",
+            Alignment_types.supplementary_forward => "supplementary_forward",
+            Alignment_types.supplementary_reverse => "supplementary_reverse",
+            Alignment_types.unmapped => "unmapped",
+        };
+    }
+};
+
+pub fn set_alignment(flag: u16) !Alignment_types {
+    return switch (flag) {
+        0 => Alignment_types.primary_forward,
+        4 => Alignment_types.unmapped,
+        16 => Alignment_types.primary_reverse,
+        256 => Alignment_types.secondary_forward,
+        272 => Alignment_types.secondary_reverse,
+        2048 => Alignment_types.supplementary_forward,
+        2064 => Alignment_types.supplementary_reverse,
+        else => error.AlignmentTypeError,
+    };
+}
+
+pub fn main(init: std.process.Init) !void {
+
+    // do not want htslib logging to print messages
+    c.hts_set_log_level(c.HTS_LOG_OFF);
+
+    const arena = init.arena.allocator();
+    const args = try init.minimal.args.toSlice(arena);
+
+    if (args.len != 2) {
+        std.debug.print("usage: {s} <file.bam>\n", .{args[0]});
+        std.process.exit(1);
+    }
+
+    const path = args[1];
+
+    const fp = c.sam_open(path.ptr, "r");
+    if (fp == null) return error.OpenFailed;
+    defer _ = c.sam_close(fp);
+
+    const hdr = c.sam_hdr_read(fp);
+    if (hdr == null) return error.HeaderReadFailed;
+    defer c.sam_hdr_destroy(hdr);
+
+    const rec = c.bam_init1() orelse return error.RecordAllocFailed;
+    defer c.bam_destroy1(rec);
+
+    var count: u64 = 0;
+    var align_len: i64 = 0;
+    var seq_len: i32 = 0;
+
+    std.debug.print("{s}", .{"read_id\talign_length\tsequence_length_template\talignment_type"});
+
+    while (c.sam_read1(fp, hdr, rec) >= 0) {
+        const rec_view: *align(1) const Bam1CoreView = @ptrCast(rec);
+        align_len = if ((rec_view.core.flag & c.BAM_FUNMAP) != 0)
+            0
+        else
+            c.bam_endpos(rec) - rec_view.core.pos;
+        seq_len = rec_view.core.l_qseq;
+
+        // simple assertions for validity
+        assert(align_len >= 0);
+        assert(seq_len >= 0);
+        assert(rec_view.core.l_qname > 0);
+        assert(rec_view.core.l_qname > rec_view.core.l_extranul);
+
+        // increment counter and check it is not too large
+        count += 1;
+        if (count > std.math.maxInt(u32)) return error.TooManyRecords;
+
+        std.debug.print("{s}\t{d}\t{d}\t{s}\n", .{ rec_view.data[0 .. rec_view.core.l_qname - rec_view.core.l_extranul], align_len, seq_len, blk: {
+            const b = set_alignment(rec_view.core.flag) catch |err| {
+                return err;
+            };
+            break :blk b.name();
+        } });
+    }
+
+    assert(count > 0);
+}
